9e8f464f91f9d21e99e1e306ec0623c63df9d3ce
David Zimmerman <david.zimmerman@cfpb.gov>
2016-04-15 14:14:25 -0400

    Fixing a price finding error I introduced

--- dimension/models.py
+++ dimension/models.py
@@ -38,15 +38,15 @@ class Subsession(BaseSubsession):
         # Set which game number will be displayed to participants
         game = math.floor(self.round_number/Constants.rounds_per_game)+1
         # Set the number of prices used for each product in the game
-        if game ==1:
-            num_prices = Constants.prices_per_seller[0]
-        elif game == 2:
-            num_prices = Constants.prices_per_seller[1]
-        else:
-            num_prices = Constants.prices_per_seller[2]
+        # if game ==1:
+        #     num_prices = Constants.prices_per_seller[0]
+        # elif game == 2:
+        #     num_prices = Constants.prices_per_seller[1]
+        # else:
+        #     num_prices = Constants.prices_per_seller[2]
         
         for i, group in enumerate(self.get_groups()):
-            
+            group.num_prices = 2
             group_roles = self.generate_roles()
             for player, role in zip(group.get_players(), group_roles):
                 player.participant.vars['role'] = role
@@ -67,6 +67,7 @@ class Player(BasePlayer):
     buyer_choice = models.PositiveIntegerField(null=True, blank=True)
     buyer_total = models.CurrencyField(null=True, blank=True)
 
+    seller_price0 = models.CurrencyField(null=True, blank=True)
     seller_price1 = models.CurrencyField(null=True, blank=True)
     seller_price2 = models.CurrencyField(null=True, blank=True)
     seller_price3 = models.CurrencyField(null=True, blank=True)
@@ -74,4 +75,3 @@ class Player(BasePlayer):
     seller_price5 = models.CurrencyField(null=True, blank=True)
     seller_price6 = models.CurrencyField(null=True, blank=True)
     seller_price7 = models.CurrencyField(null=True, blank=True)
-    seller_price8 = models.CurrencyField(null=True, blank=True)
