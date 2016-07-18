518ceca9cd9699e7f00071cb5b01d8f697db1930
David Zimmerman <david.zimmerman@cfpb.gov>
2016-07-18 14:38:47 -0400

    Added intnral funcationality to allow for calculation of profits, different conditions, assignment of role identifiers within groups
    I added several functions to the player class so that the cost to buyers,
    total seller price, number of sales made, and profit are all calculated at
    the end of each round.  Individuals are assigned to roles and for each
    individual sharing a role within a group they are randomly assigned either
    a value of 1 or 2 in order to identify the particular player within the
    round.
    
    The game now lets you specify the number of practice rounds and the number
    of games plus the total number of non-practice round games.

--- dimension/tests.py
+++ dimension/tests.py
@@ -12,25 +12,43 @@ from .models import Constants
 
 class PlayerBot(Bot):
     def play_round(self):
+        if self.subsession.round_number == 1:
+            self.submit(views.Begin)
+            self.submit(views.PRA)
+
         self.submit(views.Introduction)
-        self.submit(views.Introduction1)
-        self.submit(views.Introduction2)
-        self.submit(views.Introduction3)
-        self.submit(views.GameInstruction)
-        self.submit(views.GameInstructions2)
+        self.submit(views.IntroductionPayment)
+        self.submit(views.IntroductionRoles)
+        self.submit(views.AssignedDirections)
+        self.submit(views.SellerInstructions)
+        self.submit(views.BuyerInstructions)
+        self.submit(views.RoundSummaryExample)
+        self.submit(views.Intro)
+
+        if self.player.role_int == 1:
+            if self.subsession.num_prices == 1:
+                price = random.randint(0,400)
+                self.submit(views.SetPrices, {'seller_price0': price})
+            elif self.subsession.num_prices==4:
+                prices = [random.randint(0,400/2) for x in xrange(4)]
+                self.submit(views.SetPrices, {'seller_price0' : prices[0],
+                    'seller_price1' : prices[1], 'seller_price2' : prices[2],
+                    'seller_price3' : prices[3]})
+            else:
+                prices = [random.randint(0,400/4) for x in xrange(8)]
+                self.submit(views.SetPrices, {'seller_price0' : prices[0],
+                    'seller_price1' : prices[1], 'seller_price2' : prices[2],
+                    'seller_price3' : prices[3], 'seller_price4' : prices[4],
+                    'seller_price5' : prices[5], 'seller_price6' : prices[6],
+                    'seller_price7' : prices[7]})
+        else:
+            choice = random.choice([0,1])
+            self.submit(views.SelectSeller, {'buyer_choice': choice})
+
+        self.submit(views.RoundSummary)
+
+                
         
     def validate_play(self):
         pass
 
-
-    Introduction,
-    Introduction1,
-    Introduction2,
-    Introduction3,
-    GameInstruction,
-    GameInstructions2,
-    SetPrices,
-    BuyerWaitPage,
-    SelectSeller,
-    ResultsWaitPage,
-    Results,
