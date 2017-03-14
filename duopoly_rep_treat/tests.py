f3011144851088dd9a83faf183ac0c33ba33848e
Banker <Bankerm@cfpb.local>
2017-03-14 17:13:25 -0400

    Changed tests.py for updated game

--- duopoly_rep_treat/tests.py
+++ duopoly_rep_treat/tests.py
@@ -10,38 +10,34 @@ class PlayerBot(Bot):
     # NOTE: To turn on browser bots, go to settings.py and set field 'use_browser_bots' to true for both survey and duopoly_rep_treat
     def play_round(self):
         # Going through instruction pages in beginning of the game
-        if (self.subsession.round_number == 1):
+        if (self.subsession.show_instructions_base):
+          #yield Submission(views.WaitStartInstructions, check_html = False)
           yield Submission(views.IntroductionSplash, check_html = False) # Different syntax for splash pages (don't have 'next' buttons)
           yield(views.IntroductionPRA)
           yield(views.InstructionsBasics)
-          yield(views.InstructionsPayment)
+          yield(views.InstructionsBasicsQuiz)
           yield(views.InstructionsRoles)
-          yield(views.InstructionsSeller)
-          yield(views.InstructionsSellerPrices)
-          yield(views.InstructionsSellerQ1)
-          yield(views.InstructionsSellerQ1Ans)
-          yield(views.InstructionsSellerQ2)
-          yield(views.InstructionsSellerQ2Ans)
-          yield(views.InstructionsBuyer)
-          yield(views.InstructionsRoundResults)
-          yield(views.InstructionsWaitGame)
-          yield(views.InstructionsCleanUp)
-          yield(views.PracticeBegin)  
+          yield(views.InstructionsRolesQuiz)
 
         # New treatment page only comes in between two treatments for paid rounds
-        if (self.subsession.round_number > Constants.num_rounds_practice + 1) and ((self.subsession.round_number - 1) % Constants.num_rounds_treatment == 0):
+        if (self.subsession.show_instructions_block and not self.subsession.show_instructions_base):
           yield(views.InstructionsNewTreatment)
 
-        # Reviews instructions once after first round
-        first = self.subsession.treatment_first_multiple or self.subsession.treatment_first_singular
-        if (self.subsession.round_number != 1 and (self.subsession.block_new and first)):
+        if (self.subsession.show_instructions_roles):
           yield(views.InstructionsSeller)
-          yield(views.InstructionsSellerPrices)
+          yield(views.InstructionsSellerQuiz)
           yield(views.InstructionsBuyer)
+          yield(views.InstructionsBuyerQuiz)
 
-        # Pass through splash screen between practice and paid rounds
-        if ((self.subsession.round_number == Constants.num_rounds_practice + 1) or (self.subsession.block_new and not self.subsession.round_number == 1)):
-          yield Submission(views.PracticeEnd, check_html = False)
+        if (self.subsession.show_instructions_base):
+          yield(views.InstructionsRoundResults)
+          yield(views.InstructionsWaitGame)
+
+        if (self.subsession.show_instructions_practice):
+          yield(views.PracticeBegin)
+        if (self.subsession.show_instructions_real):
+          yield Submission(views.PracticeEnd, check_html = False)  
+
 
         # Set and submit ask prices if participant is a seller this round
         if (self.player.seller_bool):
